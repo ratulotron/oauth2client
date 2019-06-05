4a3f573509f3c8b40260ace8a52ade5b55108a69
Ratul Minhaz <minhaz.ratul@gmail.com>
2019-06-06 04:15:40 +0600

    Initial commit

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,5 @@ AUTHENTICATION_BACKENDS = (
 )
 
 OAUTH_SERVER_BASEURL = 'http://localhost:8000'
+
+django_heroku.settings(locals())
